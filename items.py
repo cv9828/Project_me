983c8b45bfcc159cc351c883f7903eba6d2ab840
Charleston Victorino <cv9828@hawaii.edu>
2015-12-01 20:02:07 -1000

    seprated data into item.py

---
+++ items.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+data = {
+    "title": u"Learning C++ for beginners",
+    "description": u"Learning to code In C++?  Here’s a few links where it will all help you get started.",
+    "items": [
+        {"title": u"MODULE ONE: “Choose a complier”", 
+         "youtube_link": "https://www.youtube.com/watch?v=tvC1WCdV1XU", 
+         "description": u"""Here will be between 1-5 sentences about the video discussing about on what the user
+    should expect to know after watching it. Here will be between 1-5 sentences about  the video 
+    discussing about on <i>what the user should expect to know after watching it</i>. <br />
+    Here will be between 1-5 sentences   about the video discussing about on what the user
+    should expect to know after watching it. <br />
+        Here will be between 1-5 sentences about the video 
+    discussing about on what the user should expect to know after watching it.
+    """},
+        {"title": u"MODULE TWO: “Hello World!”", 
+         "youtube_link": "https://www.youtube.com/watch?v=SWZfFNyUsxc", 
+         "description": u"""
+         Here will be between 1-5 sentences about the video discussing about on what the user
+    should expect to know after watching it. Here will be between 1-5 sentences about  the video 
+    discussing about on what the user <code>python is cool</code> should expect to know after watching it. <br />
+    Here will be between 1-5 sentences   about the video discussing about on what the user
+    should expect <b>to know after watching it</b>. Here will be between 1-5 sentences about the video 
+    discussing about on what the user should expect to know after watching it.
+         """},
+        {"title": u"MODULE TWO: “Hello World!”",
+         "youtube_link": "https://www.youtube.com/watch?v=SWZfFNyUsxc",
+         "description": u"""
+         Here will be between 1-5 sentences about the video discussing about on what the user
+            should expect to know after watching it. Here will be between 1-5 sentences about  the video 
+            discussing about on what the user <code>python is cool</code> should expect to know after watching it. <br />
+            Here will be between 1-5 sentences   about the video discussing about on what the user
+            should expect <b>to know after watching it</b>. Here will be between 1-5 sentences about the video 
+            discussing about on what the user should expect to know after watching it.
+         """}    
+    ]
+}
--- tutorialweb.py
+++ tutorialweb.py
@@ -3,6 +3,7 @@
 
 import BaseHTTPServer
 import codecs
+import items
 
 class MyCustomHTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     data = {}
@@ -34,30 +35,5 @@ def main(address, port, data):
     httpd.serve_forever()
 
 if __name__ == "__main__":
-    data = {
-        "title": u"Learning C++ for beginners",
-        "description": u"Learning to code In C++?  Here’s a few links where it will all help you get started.",
-        "items": [
-            {"title": u"MODULE ONE: “Choose a complier”", 
-             "youtube_link": "https://www.youtube.com/watch?v=tvC1WCdV1XU", 
-             "description": u"""Here will be between 1-5 sentences about the video discussing about on what the user
-		should expect to know after watching it. Here will be between 1-5 sentences about  the video 
-		discussing about on <i>what the user should expect to know after watching it</i>. <br />
-		Here will be between 1-5 sentences   about the video discussing about on what the user
-		should expect to know after watching it. <br />
-        Here will be between 1-5 sentences about the video 
-		discussing about on what the user should expect to know after watching it.
-        """},
-            {"title": u"MODULE TWO: “Hello World!”", 
-             "youtube_link": "https://www.youtube.com/watch?v=SWZfFNyUsxc", 
-             "description": u"""
-             Here will be between 1-5 sentences about the video discussing about on what the user
-		should expect to know after watching it. Here will be between 1-5 sentences about  the video 
-		discussing about on what the user <code>python is cool</code> should expect to know after watching it. <br />
-		Here will be between 1-5 sentences   about the video discussing about on what the user
-		should expect <b>to know after watching it</b>. Here will be between 1-5 sentences about the video 
-		discussing about on what the user should expect to know after watching it.
-             """},    
-        ]
-    }
-    main("0.0.0.0", 8080, data)
+    #using data varible imported from items file
+    main("0.0.0.0", 8080, items.data)
